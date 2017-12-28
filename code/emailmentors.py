4e7ed5a94a3e1fc9c26b8f97cf5c1ad83994db73
Owen W. Taylor <otaylor@fishsoup.net>
2017-12-27 21:33:10 -0500

    emailmentors.py: Add support for sending mails out via SMTP
    Example:
     ./emailmentors.py testemail_out testemail_body testemail_contacts
     # check that the mails in testemail_out look OK
     SMTP_USER=<user> SMTP_PASSWORD=<password> ./emailmentors.py testemail_out testemail_body testemail_contacts --smtp-server mail.outreachy.org --smtp-ssl --smtp-login

--- code/emailmentors.py
+++ code/emailmentors.py
@@ -16,8 +16,12 @@
 
 import argparse
 import os
+import re
+import smtplib
+import sys
 import csv
 
+from_address = 'organizers@outreachy.org'
 header1 = '''From: Outreachy Organizers <organizers@outreachy.org>
 '''
 
@@ -43,6 +47,13 @@ def main():
     parser = argparse.ArgumentParser(description='Generate several text-based emails to a mentor about their intern')
     parser.add_argument('outdir', help='Directory to create draft emails in')
     parser.add_argument('body', help='File containing the text-based body of the email', type=argparse.FileType('r'))
+    parser.add_argument('--smtp-server', help='SMTP server to send mail via')
+    parser.add_argument('--smtp-port', help='Port to use on SMTP server (default is 25 for non-ssl, 465 for SSL)', default=0, type=int)
+    parser.add_argument('--smtp-ssl', help='Connect to the SMTP server via SSL',
+                        default=False, action='store_true')
+    parser.add_argument('--smtp-login', help='Login to the SMTP server. ' +
+                        'SMTP USER and SMTP PASSWORD must be set in the environment',
+                        default=False, action='store_true')
     parser.add_argument('--include-coordinator', help='Include coordinator on the email?',
                         default=False, action='store_true')
     parser.add_argument('--include-intern', help='Include coordinator on the email?',
@@ -105,20 +116,52 @@ def main():
         if line == '':
             break
 
+    if args.smtp_server:
+        if args.smtp_ssl:
+            port = args.smtp_port if args.smtp_port else 465
+            smtp = smtplib.SMTP_SSL(args.smtp_server, port=port)
+        else:
+            port = args.smtp_port if args.smtp_port else 25
+            smtp = smtplib.SMTP(args.smtp_server, port=port)
+
+        if args.smtp_login:
+            try:
+                user = os.environ['SMTP_USER']
+                password = os.environ['SMTP_PASSWORD']
+            except KeyError:
+                print('SMTP_USER and SMTP_PASSWORD must be set in the environment when --smtp-login is specified', file=sys.stderr)
+                sys.exit(1)
+
+            smtp.login(user, password)
+    else:
+         smtp = None
+
     body = args.body.read()
     for pair in cohort_pairs:
         intern_name = pair.intern_contact.split('<', 1)[0].strip()
         filename = intern_name.replace(' ', '-') + '.txt'
         this_body = body.replace('$INTERN', intern_name)
-        with open(os.path.join(args.outdir, filename), 'w') as outfile:
+        mailfile = os.path.join(args.outdir, filename)
+
+        to_line = pair.mentor_contacts
+        if args.include_coordinator:
+            to_line += ',\n\t' + pair.coordinator_contacts
+        if args.include_intern:
+            to_line += ',\n\t' + pair.intern_contact
+
+        with open(mailfile, 'w') as outfile:
             outfile.write(header1)
-            outfile.write('To: ' + pair.mentor_contacts + '\n')
-            if args.include_coordinator:
-                outfile.write('\t' + pair.coordinator_contacts + '\n')
-            if args.include_intern:
-                outfile.write('\t' + pair.intern_contacts + '\n')
+            outfile.write('To: ' + to_line + '\n')
             outfile.write(this_body)
             outfile.write(signature)
+
+        if smtp:
+            to_addresses = re.split(r'\s*,\s*', to_line)
+
+            with open(mailfile, 'r') as infile:
+                msg = infile.read()
+
+            smtp.sendmail(from_address, to_addresses, msg)
 
 if __name__ == "__main__":
     main()
