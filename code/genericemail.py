a4c86432b4bd7af7f8076504d55b5d9f6df15a81
Sage Sharp <sharp@otter.technology>
2022-04-13 05:12:44 -0700

    Rewrite script to generate semi-personalized mass emails.
    Revise the script to be more generic, taking positional arguments for
    things like from headers and a subject line.
    
    Make the script parse a message body file, and replace the phrase "Hi
    NAME," with either the recipient's first name or the phrase "Greetings,"
    
    The first name is assumed to be the first word in the Name row in the
    recipient CSV file. That's not always true, depending on which culture
    the recipient lives in. You may need to either modify the CSV file, or
    edit the greetings line if the culture lists last name as the first
    word of a full name.
    
    Add more descriptive help text.
    
    Update the copyright to reflect my name change and official Conservancy
    email address. As critters say, 🎶 "It's been a while..." 🎶

--- code/genericemail.py
+++ code/genericemail.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2017 Sarah Sharp <sharp@otter.technology>
+# Copyright 2022 Sage Sharp <sage@sfconservancy.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,37 +14,59 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Create a set of generic form emails when we don't have a resume match.
 
 import argparse
 import csv
 import os
-from resumesearch import header1, header3, atBooth, generalInfo, moreInfo
 
 def main():
-    parser = argparse.ArgumentParser(description='Search text resume files for skillset matches.')
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description='Create files to send mass (semi-personalized) emails with mutt.',
+            epilog='''
+This script will replace the string "Hi NAME," in the message body
+with the first name of the recipient, or "Greetings," if there is no name.
+
+You can send these emails by changing to the directory with the email files,
+and running this command:
+
+$ for i in `ls .`; do mutt -F ~/.muttrc-outreachy -H $i; rm $i; done
+
+If you want to test what the command would do, you can run:
+
+$ for i in `ls .`; do echo mutt -F ~/.muttrc-outreachy -H $i; echo rm $i; done
+''',
+            )
     parser.add_argument('outdir', help='Directory to create form emails in')
-    parser.add_argument('csv', help='CSV file of people who stopped by the booth')
+    parser.add_argument('csv', help='CSV file of people to email')
+    parser.add_argument('fromemail', help='Name and email address to use in the From header')
+    parser.add_argument('subject', help='Subject line of email')
+    parser.add_argument('messagefile', help='File containing the message body of email')
     args = parser.parse_args()
 
     if not os.path.exists(args.outdir):
             os.makedirs(args.outdir)
 
-    tosend = []
+    body = ''
+    with open(args.messagefile, 'r') as messageFile:
+        body = messageFile.read()
+
+    count = 0
     with open(args.csv, 'r') as csvFile:
         freader = csv.DictReader(csvFile, delimiter=',', quotechar='"')
         for row in freader:
-            if not row['Tapia resume database?']:
-                tosend.append('"' + row['Name'] + '" <' + row['Email'] + '>')
+            with open(os.path.join(args.outdir, str(count) + '.txt'), 'w') as email:
+                email.write('From: ' + args.fromemail + '\n')
+                email.write('To: ' + '"' + row['Name'] + '" <' + row['Email'] + '>' + '\n')
+                email.write('Subject: ' + args.subject + '\n')
+                email.write('\n')
+                if row['Name']:
+                    email.write(body.replace('Hi NAME,', 'Hi ' + row['Name'].split(' ')[0] + ','))
+                else:
+                    email.write(body.replace('Hi NAME,', 'Greetings,'))
+                count += 1
 
-    for index, contact in enumerate(tosend):
-        with open(os.path.join(args.outdir, str(index) + '.txt'), 'w') as email:
-            email.write(header1)
-            email.write('To: ' + contact + '\n')
-            email.write(header3)
-            email.write(atBooth + generalInfo + moreInfo)
-    print('Wrote', len(tosend), 'resume draft emails to', args.outdir)
+    print('Wrote ', count + 1, ' draft emails to ', args.outdir)
 
 if __name__ == "__main__":
     main()
